8ac62527b6ecd05f408821d3b8df6b111e31acac
Cameron Peterson <86241273+campeterson41@users.noreply.github.com>
2023-07-15 15:18:24 -0400

    Update Data Scraping Project - Wikipedia.py

--- Data Scraping Project - Wikipedia.py
+++ Data Scraping Project - Wikipedia.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
+# Scraping and exporting .csv file from https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue
 
 from bs4 import BeautifulSoup
 import requests
-
-
-# In[3]:
-
 
 url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
 
@@ -17,46 +9,18 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html')
 
-
-# In[16]:
-
-
 table = soup.find_all('table')[1]
 
-
-# In[17]:
-
-
 world_titles = table.find_all('th')
-
-
-# In[18]:
-
 
 world_table_titles = [title.text.strip() for title in world_titles]
 print(world_table_titles)
 
-
-# In[19]:
-
-
 import pandas as pd
-
-
-# In[20]:
-
 
 df = pd.DataFrame(columns = world_table_titles)
 
-
-# In[23]:
-
-
 column_data = table.find_all('tr')
-
-
-# In[25]:
-
 
 for row in column_data[1:]:
     row_data = row.find_all('td')
@@ -64,22 +28,7 @@ for row in column_data[1:]:
 
     length = len(df)
     df.loc[length] = individual_row_data
-
-
-# In[26]:
-
-
+    
 df
 
-
-# In[27]:
-
-
 df.to_csv(r'/Users/cameronpeterson/Documents/Sorting folder/Companies.csv', index = False)
-
-
-# In[ ]:
-
-
-
-
